27b5aaec7c1149128dfc2760d4298268677726d6
Prokhorenko Vladyslav <38713205+prokhorenko-vladyslav@users.noreply.github.com>
2020-07-23 23:29:01 +0300

    Updated deletable trait

--- src/Traits/Deletable.php
+++ src/Traits/Deletable.php
@@ -4,7 +4,21 @@
 namespace Laurel\Deletable\Traits;
 
 
+use Laurel\Deletable\Exceptions\ModelCannotBeDeletedException;
+
 trait Deletable
 {
+    public function deleteIf() : bool
+    {
+        return true;
+    }
 
+    public function delete()
+    {
+        if ($this->deleteIf()) {
+            return parent::delete();
+        } else {
+            throw new ModelCannotBeDeletedException("Model " . static::class . "cannot be deleted");
+        }
+    }
 }
